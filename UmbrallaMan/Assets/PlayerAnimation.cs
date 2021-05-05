6b4a1ee8ffe53feec26cde7f2a1e60718e318758
elor504 <74613768+elor504@users.noreply.github.com>
2021-05-05 14:28:42 +0300

    added player gethit gfx

---
+++ UmbrallaMan/Assets/PlayerAnimation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+	//
+	[SerializeField]
+    SpriteRenderer sprite;
+
+	[SerializeField]
+	Color HitColor;
+	[SerializeField]
+	Color NormalColor;
+
+	public void HitGFX()
+	{
+		StartCoroutine(HitEffects());
+	}
+
+	public IEnumerator HitEffects()
+	{
+		sprite.color = HitColor;
+		yield return new WaitForSeconds(0.1f);
+		sprite.color = NormalColor;
+		yield return new WaitForSeconds(0.1f);
+		sprite.color = HitColor;
+		yield return new WaitForSeconds(0.1f);
+		sprite.color = NormalColor;
+		yield return new WaitForSeconds(0.1f);
+		sprite.color = HitColor;
+		yield return new WaitForSeconds(0.1f);
+		sprite.color = NormalColor;
+		yield return new WaitForSeconds(0.1f);
+		sprite.color = HitColor;
+		yield return new WaitForSeconds(0.1f);
+		sprite.color = NormalColor;
+
+		//player can be hitted again, need to add a bool to the player so he would not take damage while this effects is activated
+	}
+
+
+	
+
+
+
+
+
+}
